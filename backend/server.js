e83b762402d9a3930a18af88e26d63b6c4c731b0
saiprasadkale <saiprasadkale@gmail.com>
2024-02-03 20:28:13 +0530

    Basic node server. Basic React app setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const app = express();
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.filter((data) => {
+    return data._id === req.params.id;
+  });
+  res.send(singleChat);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
